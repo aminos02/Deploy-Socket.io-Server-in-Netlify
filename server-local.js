7114a03f079908041cfc27d2d0346ce70dea7dbd
Azzouz Mohammed el amin <39242920+aminos02@users.noreply.github.com>
2021-09-30 18:11:25 +0200

    v2

--- server-local.js
+++ server-local.js
@@ -1,5 +1,49 @@
-'use strict';
+"use strict";
 
-const app = require('./express/server');
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "https://aminosocial-ff491.web.app",
+  },
+});
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
