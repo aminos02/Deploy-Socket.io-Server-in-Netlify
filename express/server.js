b0ac30c8f3498b2e3931fa56db894796ad01c163
Azzouz Mohammed el amin <39242920+aminos02@users.noreply.github.com>
2021-09-30 18:15:36 +0200

    v3

--- express/server.js
+++ express/server.js
@@ -1,5 +1,53 @@
 "use strict";
-const express = require("express");
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "https://aminosocial-ff491.web.app",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+/*const express = require("express");
 const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
@@ -20,3 +68,4 @@ app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
+*/
--- server-local.js
+++ server-local.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const io = require("socket.io")(8900, {
   cors: {
     origin: "https://aminosocial-ff491.web.app",
@@ -47,3 +46,26 @@ io.on("connection", (socket) => {
     io.emit("getUsers", users);
   });
 });
+
+/*const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Expre.js!</h1>");
+  res.end();
+});
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+module.exports = app;
+module.exports.handler = serverless(app);
+*/
